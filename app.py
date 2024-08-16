fc702b7f9811274c9a7798dcb8e65530b8f9b261
aeirich <andreas.eirich1@web.de>
2024-08-16 15:34:34 +0200

    Kleinigkeiten

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import streamlit as st
+import pandas as pd
 
-st.write('hello world')
+pd.set_option('display.width', 1000)
+pd.set_option('display.max_columns', None)
+st.set_page_config(page_title='ImmoLytics',page_icon=":house_buildings:", layout="wide", initial_sidebar_state="auto", menu_items=None)
+st.title('FinaLytics `v1.0`')
+
 
 uploaded_files = st.file_uploader(
     "Choose a CSV file", accept_multiple_files=True
---
+++ functions.py
@@ -0,0 +1,12 @@
+def colorset():
+    color_dict = {
+       'darkorange':'rgb(255,140,0)',
+       'darkorange4':'rgb(139,69,0)',
+       'gold':'rgb(255,215,0)',
+       'PeachPuff':'rgb(255,218,185)',
+       'honeydew3':'rgb(193,205,193)',
+       'lightpink3':'rgb(205,140,149)',
+       'orangered':'rgb(255,69,0)',
+       'gold':'rgb(255,215,0)'}
+    colors = list(color_dict.values())
+    return colors
